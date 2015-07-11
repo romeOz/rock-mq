d77ddbaae5fe9e78ec8de4b3758e504c4b6805b2
romeOz <serggalka@gmail.com>
2015-07-11 16:39:17 +0300

    Use docker to travis.

--- tests/data/travis/zmq-setup.sh
+++ tests/data/travis/zmq-setup.sh
@@ -5,21 +5,21 @@ if (php --version | grep -i HipHop > /dev/null); then
     exit 0
 fi
 
-sudo add-apt-repository -y ppa:chris-lea/zeromq
-#sudo add-apt-repository -y ppa:ondrej/php5
-sudo apt-get update
-
-# Install ZeroMQ
-sudo apt-get install -y pkg-config libzmq3-dev libpgm-5.1-0
-#wget http://download.zeromq.org/zeromq-4.0.4.tar.gz
-#tar -xf zeromq-4.0.4.tar.gz
-#cd zeromq-4.0.4
-#./configure
-#make
-#sudo make install
-#cd -
-#yes | pecl install zmq-beta
-# compile manually, because `pecl install apcu-beta` keep asking questions
+#sudo add-apt-repository -y ppa:chris-lea/zeromq
+##sudo add-apt-repository -y ppa:ondrej/php5
+#sudo apt-get update
+#
+## Install ZeroMQ
+#sudo apt-get install -y pkg-config libzmq3-dev libpgm-5.1-0
+##wget http://download.zeromq.org/zeromq-4.0.4.tar.gz
+##tar -xf zeromq-4.0.4.tar.gz
+##cd zeromq-4.0.4
+##./configure
+##make
+##sudo make install
+##cd -
+##yes | pecl install zmq-beta
+## compile manually, because `pecl install apcu-beta` keep asking questions
 git clone https://github.com/mkoppanen/php-zmq.git
 cd php-zmq
 phpize && ./configure && make && make install && echo "Installed ext/php-zmq-dev"
